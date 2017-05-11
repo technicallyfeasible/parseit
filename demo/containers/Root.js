f3d3e177fea28eb0cac01f49842ab23f20997cce
Matt Bilbow <hi@mattbilbow.co.uk>
2017-05-11 14:07:36 +0100

    Starting to add some UI

--- demo/containers/Root.js
+++ demo/containers/Root.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Header from '../components/Header';
+import Hero from '../components/Hero';
 import DataParserTest from './DataParserTest';
 
 
 export default function Root() {
   return (
     <div>
+      <Header />
+      <Hero />
       <DataParserTest />
     </div>
   );
