ff885bd0cf55aed9ae70387a382f3f354db1565a
Jens <jens.elstner@technicallyfeasible.com>
2017-05-18 18:45:21 +0200

    Try to make heroku run

--- bin/demo.js
+++ bin/demo.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 app.use('/', express.static(path.join(__dirname, '../release/demo')));
 app.use('/demo', express.static(path.join(__dirname, '../release/demo')));
-const server = app.listen(80, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`http://localhost:${server.address().port}/`);
 });
