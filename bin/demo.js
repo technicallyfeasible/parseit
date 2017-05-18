0a9060034f873ba5139f402775f4a81e09ed79c1
Jens <jens.elstner@technicallyfeasible.com>
2017-05-18 18:06:06 +0200

    Setup demo page compilation and hosting

---
+++ bin/demo.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+app.use('/', express.static(path.join(__dirname, '../release/demo')));
+app.use('/demo', express.static(path.join(__dirname, '../release/demo')));
+const server = app.listen(80, () => {
+  console.log(`http://localhost:${server.address().port}/`);
+});
